cfb926d4b731c5513709a97955b91ca6de86eb90
WuhuFlying <360786367@qq.com>
2021-10-22 01:04:05 +0800

    add:多人对战游戏大厅

--- pages/gamehall/gamehall.js
+++ pages/gamehall/gamehall.js
@@ -5,45 +5,196 @@ Page({
      * 页面的初始数据
      */
     data: {
-        roomList:[
-            {
-                "uuid":"",
-                "host_id":"",
-                "client_id":"",
-                "create_at":""
+        roomList:[],
+        currentPage:1,
+        uuid:"",
+        isPrivate:false,
+    },
+    
+    //加入房间
+    joinRoom:function(para){
+        var roomNumber=para.currentTarget.dataset.number;
+        var uuid=this.data.roomList[roomNumber].uuid;
+        console.log(uuid);
+        var token=wx.getStorageSync("token");
+        wx.request({
+            url:"http://172.17.173.97:9000/api/game/:uuid",
+            method:"POST",
+            data:{
+                uuid:uuid,
             },
-            {
-                "uuid":"",
-                "host_id":"",
-                "client_id":"",
-                "create_at":""
+            header: {
+                "Content-Type" : 'application/x-www-form-urlencoded ',
+                "Authorization": token
             },
-            {
-                "uuid":"",
-                "host_id":"",
-                "client_id":"",
-                "create_at":""
+            success(res){
+                console.log(res.data);
+                if(res.data.code=="200"){
+                    // wx.redirectTo({
+                    //     url:"/pages/PVP/PVP"
+                    // })
+                    console.log("成功!")
+                }
+                else{
+                    wx.showToast({
+                        title: '加入失败，请重试',
+                        icon: 'none',
+                        duration: 1500
+                    })
+                }
             },
-            {
-                "uuid":"",
-                "host_id":"",
-                "client_id":"",
-                "create_at":""
+            fail(res){
+                wx.showToast({
+                    title: '网络错误，请重试',
+                    icon: 'none',
+                    duration: 1500
+                })
+            }
+        })
+    },
+    
+    //通过uuid加入房间
+    enterRoom:function(){
+        var uuid=this.data.uuid;
+        var token=wx.getStorageSync("token");
+        var path = "http://172.17.173.97:9000/api/game/:"+uuid;
+        console.log(uuid)
+        console.log(path);
+        wx.request({
+            url:"http://172.17.173.97:9000/api/game/:uuid",
+            method:"POST",
+            data:{
+                uuid:uuid
             },
-            {
-                "uuid":"",
-                "host_id":"",
-                "client_id":"",
-                "create_at":""
+            header: {
+                "Content-Type" : 'application/x-www-form-urlencoded ',
+                "Authorization": token
             },
-            {
-                "uuid":"",
-                "host_id":"",
-                "client_id":"",
-                "create_at":""
+            success(res){
+                console.log(res.data);
+                if(res.data.code=="200"){
+                    // wx.redirectTo({
+                    //     url:"/pages/PVP/PVP"
+                    // })
+                    console.log("成功!")
+                }
+                else{
+                    wx.showToast({
+                        title: '加入失败，请重试',
+                        icon: 'none',
+                        duration: 1500
+                    })
+                }
             },
-        ],
+            fail(res){
+                wx.showToast({
+                    title: '网络错误，请重试',
+                    icon: 'none',
+                    duration: 1500
+                })
+            }
+        })
+    },
 
+    //创建房间
+    createRoom:function(){
+        var token=wx.getStorageSync("token");
+        wx.request({
+            url:"http://172.17.173.97:9000/api/game",
+            method:"POST",
+            data:{
+                private:this.data.isPrivate,
+            },
+            header: {
+                "Content-Type" : 'application/x-www-form-urlencoded ',
+                "Authorization": token
+            },
+            success(res){
+                console.log(res.data)
+            }
+        })
+    },
+
+    bandChange(e){
+        var isPrivate;
+        if(e.detail.value=="public") isPrivate=false;
+        else isPrivate=true;
+        this.setData({
+            isPrivate:isPrivate,
+        })
+    },
+
+    //上一页
+    clickPreviousPage:function(){
+        var page=this.data.currentPage-1;
+        var token=wx.getStorageSync("token");
+        var that=this;
+        page=page.toString();
+        wx.request({
+            url:"http://172.17.173.97:9000/api/game/index",
+            method:"GET",
+            data:{
+                page_size:'4',
+                page_num:page
+            },
+            header: {
+                "Content-Type" : 'application/x-www-form-urlencoded ',
+                "Authorization": token
+            },
+            success(res){
+                if(res.data.code=="200"){
+                    page=parseInt(page)
+                    that.setData({
+                        roomList:res.data.data.games,
+                        currentPage:page
+                    })
+                }
+                else{
+                    wx.showToast({
+                        title: '加入线上游戏失败，请重试',
+                        icon: 'none',
+                        duration: 1500
+                    })
+                }
+            }
+        })
+    },
+
+    //下一页
+    clickNextPage:function(){
+        var page=this.data.currentPage+1;
+        var token=wx.getStorageSync("token");
+        console.log(token);
+        var that=this;
+        page=page.toString();
+        wx.request({
+            url:"http://172.17.173.97:9000/api/game/index",
+            method:"GET",
+            data:{
+                page_size:'4',
+                page_num:page
+            },
+            header: {
+                "Content-Type" : 'application/x-www-form-urlencoded ',
+                "Authorization": token
+            },
+            success(res){
+                if(res.data.code=="200"){
+                    page=parseInt(page)
+                    that.setData({
+                        roomList:res.data.data.games,
+                        currentPage:page
+                    })
+                }
+                else{
+                    wx.showToast({
+                        title: '加入线上游戏失败，请重试',
+                        icon: 'none',
+                        duration: 1500
+                    })
+                }
+            }
+        })
     },
 
     /**
@@ -51,6 +202,9 @@ Page({
      */
     onLoad: function (options) {
         // console.log(this.data.roomList[0].uuid)
+        this.setData({
+            currentPage:1,
+        })
         var token=wx.getStorageSync("token");
         var that=this;
         //console.log(token)
@@ -58,7 +212,7 @@ Page({
             url:"http://172.17.173.97:9000/api/game/index",
             method:"GET",
             data:{
-                page_size:'6',
+                page_size:'4',
                 page_num:'1'
             },
             header: {
@@ -67,10 +221,28 @@ Page({
             },
             success(res){
                 // console.log(res);
-                that.setData({
-                    roomList:res.data.data.games,
-                })
+                if(res.data.code=="200"){
+                    that.setData({
+                        roomList:res.data.data.games,
+                    })
+                }
+                else{
+                    wx.showToast({
+                        title: '加入线上游戏失败，请重试',
+                        icon: 'none',
+                        duration: 1500
+                    })
+                }
+                
                 //console.log(that.data.roomList)
+            },
+            fail(res){
+                console.log(token+"1")
+                wx.showToast({
+                    title: '网络错误，请重试',
+                    icon: 'none',
+                    duration: 1500
+                })
             }
         })
     },
